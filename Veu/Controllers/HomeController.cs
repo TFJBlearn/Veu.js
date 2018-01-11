358b2e1eb2dc696e73d9051fd5f45dfa9e8a3c41
unknown <TFJBlearn>
2018-01-11 09:50:38 +0800

    first commit

---
+++ Veu/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Veu.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Veu()
+        {
+            return View();
+        }
+
+        public void Delete(int ID) 
+        {
+            OAEntities enty = new OAEntities();
+
+            var user = enty.Users.Where(a => a.Id == ID).FirstOrDefault();
+
+            enty.Entry(user).State = System.Data.EntityState.Deleted;
+
+            int row = enty.SaveChanges();
+
+            Response.Write(row);
+            Response.End();
+               
+        }
+
+        public JsonResult GetDate() 
+        {
+            OAEntities oa = new OAEntities();
+
+            List<Users> ulist = oa.Users.ToList();
+
+            return Json(ulist, JsonRequestBehavior.AllowGet);
+        
+        }
+
+    }
+}
---
+++ Veu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Veu.MvcApplication" Language="C#" %>
